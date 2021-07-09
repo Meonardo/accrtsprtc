35825ec0c9fad8d7322db0842a0717e2c0ad7228
Hedi <hedi@amdox>
2021-07-09 15:20:42 +0800

    Shift HTTP to WebSocket when connect to janus-gateway.

--- janus.py
+++ janus.py
@@ -3,8 +3,10 @@ import asyncio
 import logging
 import random
 import string
-import time
-import aiohttp
+
+import websockets
+import json
+from websockets.exceptions import ConnectionClosed
 
 import gi
 gi.require_version('Gst', '1.0')
@@ -35,12 +37,305 @@ camera = None
 
 pcs = set()
 
+
+import attr
+
+@attr.s
+class JanusEvent:
+    sender = attr.ib(validator=attr.validators.instance_of(int))
+
+@attr.s
+class PluginData(JanusEvent):
+    plugin = attr.ib(validator=attr.validators.instance_of(str))
+    data = attr.ib()
+    jsep = attr.ib()
+
+@attr.s
+class WebrtcUp(JanusEvent):
+    pass
+
+@attr.s
+class Media(JanusEvent):
+    receiving = attr.ib(validator=attr.validators.instance_of(bool))
+    kind = attr.ib(validator=attr.validators.in_(["audio", "video"]))
+
+    @kind.validator
+    def validate_kind(self, attribute, kind):
+        if kind not in ["video", "audio"]:
+            raise ValueError("kind must equal video or audio")
+
+@attr.s
+class SlowLink(JanusEvent):
+    uplink = attr.ib(validator=attr.validators.instance_of(bool))
+    lost = attr.ib(validator=attr.validators.instance_of(int))
+
+@attr.s
+class HangUp(JanusEvent):
+    reason = attr.ib(validator=attr.validators.instance_of(str))
+
+@attr.s(cmp=False)
+class Ack:
+    transaction = attr.ib(validator=attr.validators.instance_of(str))
+
+@attr.s
+class Jsep:
+    sdp = attr.ib()
+    type = attr.ib(validator=attr.validators.in_(["offer", "pranswer", "answer", "rollback"]))
+
+
+@attr.s
+class JanusGateway:
+    server = attr.ib(validator=attr.validators.instance_of(str))
+    _messages = attr.ib(factory=set)
+
+    async def connect(self):
+        self.conn = await websockets.connect(self.server, subprotocols=['janus-protocol'])
+        transaction = transaction_id()
+        await self.conn.send(json.dumps({
+            "janus": "create",
+            "transaction": transaction
+            }))
+        resp = await self.conn.recv()
+        print (resp)
+        parsed = json.loads(resp)
+        assert parsed["janus"] == "success", "Failed creating session"
+        assert parsed["transaction"] == transaction, "Incorrect transaction"
+        self.session = parsed["data"]["id"]
+
+    async def close(self):
+        await self.conn.close()
+
+    async def leave(self):
+        transaction = transaction_id()
+        await self.conn.send(json.dumps({
+            "janus": "destroy",
+            "session_id": self.session,
+            "transaction": transaction
+            }))
+        resp = await self.conn.recv()
+        print ("left room: ", resp)
+
+    async def attach(self, plugin):
+        assert hasattr(self, "session"), "Must connect before attaching to plugin"
+        transaction = transaction_id()
+        await self.conn.send(json.dumps({
+            "janus": "attach",
+            "session_id": self.session,
+            "plugin": plugin,
+            "transaction": transaction
+        }))
+        resp = await self.conn.recv()
+        parsed = json.loads(resp)
+        assert parsed["janus"] == "success", "Failed attaching to {}".format(plugin)
+        assert parsed["transaction"] == transaction, "Incorrect transaction"
+        self.handle = parsed["data"]["id"]
+
+    async def sendtrickle(self, candidate):
+        assert hasattr(self, "session"), "Must connect before sending messages"
+        assert hasattr(self, "handle"), "Must attach before sending messages"
+
+        transaction = transaction_id()
+        janus_message = {
+            "janus": "trickle",
+            "session_id": self.session,
+            "handle_id": self.handle,
+            "transaction": transaction,
+            "candidate": candidate
+        }
+
+        await self.conn.send(json.dumps(janus_message))
+
+    async def sendmessage(self, body, jsep=None):
+        assert hasattr(self, "session"), "Must connect before sending messages"
+        assert hasattr(self, "handle"), "Must attach before sending messages"
+
+        transaction = transaction_id()
+        janus_message = {
+            "janus": "message",
+            "session_id": self.session,
+            "handle_id": self.handle,
+            "transaction": transaction,
+            "body": body
+        }
+        if jsep is not None:
+            janus_message["jsep"] = jsep
+
+        await self.conn.send(json.dumps(janus_message))
+
+    async def keepalive(self):
+        assert hasattr(self, "session"), "Must connect before sending messages"
+        assert hasattr(self, "handle"), "Must attach before sending messages"
+
+        while True:
+            try:
+                await asyncio.sleep(10)
+                transaction = transaction_id()
+                await self.conn.send(json.dumps({
+                    "janus": "keepalive",
+                    "session_id": self.session,
+                    "handle_id": self.handle,
+                    "transaction": transaction
+                }))
+            except KeyboardInterrupt:
+                return
+
+    async def recv(self):
+        if len(self._messages) > 0:
+            return self._messages.pop()
+        else:
+            return await self._recv_and_parse()
+
+    async def _recv_and_parse(self):
+        raw = json.loads(await self.conn.recv())
+        print("Received: ", raw)
+        janus = raw["janus"]
+
+        if janus == "event":
+            return PluginData(
+                sender=raw["sender"],
+                plugin=raw["plugindata"]["plugin"],
+                data=raw["plugindata"]["data"],
+                jsep=raw["jsep"] if "jsep" in raw else None
+            )
+        elif janus == "webrtcup":
+            return WebrtcUp(
+                sender=raw["sender"]
+            )
+        elif janus == "media":
+            return Media(
+                sender=raw["sender"],
+                receiving=raw["receiving"],
+                kind=raw["type"]
+            )
+        elif janus == "slowlink":
+            return SlowLink(
+                sender=raw["sender"],
+                uplink=raw["uplink"],
+                lost=raw["lost"]
+            )
+        elif janus == "hangup":
+            return HangUp(
+                sender=raw["sender"],
+                reason=raw["reason"]
+            )
+        elif janus == "ack":
+            return Ack(
+                transaction=raw["transaction"]
+            )
+        else:
+            return raw
+
+
+class WebRTCClient:
+    def __init__(self, id_, signaling, rtsp):
+        self.id_ = id_
+        self.signaling = signaling
+        self.rtsp = rtsp
+        self.pc = None
+        self.camera = None
+
+    async def destroy(self):
+        self.camera.stop()
+        await self.signaling.leave()
+        self.pc.close()
+
+    async def handle_plugin_data(self, data):
+        if data.jsep is not None:
+            await self.handle_sdp(data.jsep)
+        if data.data["publishers"] is not None:
+            publishers = data.data["publishers"]
+            for publisher in publishers:
+                print("id: %(id)s, display: %(display)s" % publisher)
+            await self.publish()
+
+    async def handle_sdp(self, msg):
+        if 'sdp' in msg:
+            sdp = msg['sdp']
+            assert(msg['type'] == 'answer')
+            print ('Received answer:\n%s' % sdp)
+            
+            # apply answer
+            await self.pc.setRemoteDescription(
+                RTCSessionDescription(sdp=sdp, type=msg['type'])
+            )
+            for t in self.pc.getTransceivers():
+                if t.kind == "video":
+                    t.setCodecPreferences(preferences)
+
+        elif 'ice' in msg:
+            ice = msg['ice']
+            candidate = ice['candidate']
+            sdpmlineindex = ice['sdpMLineIndex']
+            self.webrtc.emit('add-ice-candidate', sdpmlineindex, candidate)
+
+    async def publish(self):
+        """
+        Send video to the room.
+        """
+
+        pc = RTCPeerConnection()
+        self.pc = pc
+
+        # configure media
+        media = {"audio": False, "video": True}
+        if self.rtsp is not None:
+            video_track = H264EncodedStreamTrack(RATE)
+            self.camera = GstH264Camera(video_track, self.rtsp)
+            pc.addTrack(video_track)
+        else:
+            raise Exception("No Media Input! Stop Now.")
+
+        # send offer
+        await pc.setLocalDescription(await pc.createOffer())
+        request = {"request": "configure"}
+        request.update(media)
+
+        sdp = {
+            "jsep": {
+                "sdp": pc.localDescription.sdp,
+                "trickle": False,
+                "type": pc.localDescription.type,
+            }
+        }
+        self.signaling.sendmessage(request, sdp)
+
+    async def loop(self, signaling, room, display):
+        await signaling.connect()
+        await signaling.attach("janus.plugin.videoroom")
+
+        loop = asyncio.get_event_loop()
+        loop.create_task(signaling.keepalive())
+
+        joinmessage = { "request": "join", "ptype": "publisher", "room": room, "pin": str(room), "display": display }
+        await signaling.sendmessage(joinmessage)
+
+        assert signaling.conn
+
+        while True:
+            try:
+                msg = await signaling.recv()
+                if isinstance(msg, PluginData):
+                    self.handle_plugin_data(msg)
+                elif isinstance(msg, Media):
+                    print (msg)
+                elif isinstance(msg, WebrtcUp):
+                    print (msg)
+                elif isinstance(msg, SlowLink):
+                    print (msg)
+                elif isinstance(msg, HangUp):
+                    print (msg)
+                elif not isinstance(msg, Ack):
+                    print(msg)
+            except (KeyboardInterrupt, ConnectionClosed):
+                return
+
+        return 0
+
 def transaction_id():
     return "".join(random.choice(string.ascii_letters) for x in range(12))
 
 class GstH264Camera:
     RTSP_PIPELINE = "rtspsrc location={} latency=0 ! rtph264depay ! queue ! h264parse ! video/x-h264,alignment=nal,stream-format=byte-stream ! appsink emit-signals=True name=h264_sink"
-    #RTSP_PIPELINE = "rtspsrc location={} latency=0 ! rtph264depay ! queue ! video/x-h264,alignment=nal,stream-format=byte-stream ! appsink emit-signals=True name=h264_sink"
 
     def __init__(self, output, rtsp):
         source = GstH264Camera.RTSP_PIPELINE.format(rtsp)
@@ -61,141 +356,7 @@ class GstH264Camera:
     def stop(self):
         self.pipeline.set_state(Gst.State.NULL)
 
-
-class JanusPlugin:
-    def __init__(self, session, url):
-        self._queue = asyncio.Queue()
-        self._session = session
-        self._url = url
-
-    async def send(self, payload):
-        message = {"janus": "message", "transaction": transaction_id()}
-        message.update(payload)
-        async with self._session._http.post(self._url, json=message) as response:
-            data = await response.json()
-            assert data["janus"] == "ack"
-
-        response = await self._queue.get()
-        assert response["transaction"] == message["transaction"]
-        return response
-
-
-class JanusSession:
-    def __init__(self, url):
-        self._http = None
-        self._poll_task = None
-        self._plugins = {}
-        self._root_url = url
-        self._session_url = None
-
-    async def attach(self, plugin_name: str) -> JanusPlugin:
-        message = {
-            "janus": "attach",
-            "plugin": plugin_name,
-            "transaction": transaction_id(),
-        }
-        async with self._http.post(self._session_url, json=message) as response:
-            data = await response.json()
-            assert data["janus"] == "success"
-            plugin_id = data["data"]["id"]
-            plugin = JanusPlugin(self, self._session_url + "/" + str(plugin_id))
-            self._plugins[plugin_id] = plugin
-            return plugin
-
-    async def create(self):
-        self._http = aiohttp.ClientSession()
-        message = {"janus": "create", "transaction": transaction_id()}
-        async with self._http.post(self._root_url, json=message) as response:
-            data = await response.json()
-            assert data["janus"] == "success"
-            session_id = data["data"]["id"]
-            self._session_url = self._root_url + "/" + str(session_id)
-
-        self._poll_task = asyncio.ensure_future(self._poll())
-
-    async def destroy(self):
-        if self._poll_task:
-            self._poll_task.cancel()
-            self._poll_task = None
-
-        if self._session_url:
-            message = {"janus": "destroy", "transaction": transaction_id()}
-            async with self._http.post(self._session_url, json=message) as response:
-                data = await response.json()
-                assert data["janus"] == "success"
-            self._session_url = None
-
-        if self._http:
-            await self._http.close()
-            self._http = None
-
-    async def _poll(self):
-        while True:
-            params = {"maxev": 1, "rid": int(time.time() * 1000)}
-            async with self._http.get(self._session_url, params=params) as response:
-                data = await response.json()
-                if data["janus"] == "event":
-                    plugin = self._plugins.get(data["sender"], None)
-                    if plugin:
-                        await plugin._queue.put(data)
-                    else:
-                        print(data)
-
-
-async def publish(plugin, player, rtsp):
-    """
-    Send video to the room.
-    """
-
-    pc = RTCPeerConnection()
-    pcs.add(pc)
-
-    # configure media
-    media = {"audio": False, "video": True}
-    if player is not None:
-        if player and player.audio:
-            pc.addTrack(player.audio)
-            media["audio"] = True
-
-        if player and player.video:
-            pc.addTrack(player.video)
-        else:
-            pc.addTrack(VideoStreamTrack())
-    elif rtsp is not None:
-        global camera
-        video_track = H264EncodedStreamTrack(RATE)
-        camera = GstH264Camera(video_track, rtsp)
-        pc.addTrack(video_track)
-    else:
-        raise Exception("No Media Input! Stop Now.")
-
-    # send offer
-    await pc.setLocalDescription(await pc.createOffer())
-    request = {"request": "configure"}
-    request.update(media)
-    response = await plugin.send(
-        {
-            "body": request,
-            "jsep": {
-                "sdp": pc.localDescription.sdp,
-                "trickle": False,
-                "type": pc.localDescription.type,
-            },
-        }
-    )
-
-    # apply answer
-    await pc.setRemoteDescription(
-        RTCSessionDescription(
-            sdp=response["jsep"]["sdp"], type=response["jsep"]["type"]
-        )
-    )
-    for t in pc.getTransceivers():
-        print(t)
-        if t.kind == "video":
-            t.setCodecPreferences(preferences)
-
-
+# 暂时保留, 如有录制需求在调用
 async def subscribe(session, room, feed, recorder):
     pc = RTCPeerConnection()
     pcs.add(pc)
@@ -236,45 +397,11 @@ async def subscribe(session, room, feed, recorder):
     await recorder.start()
 
 
-async def run(player, rtsp, recorder, room, session, display):
-    await session.create()
-
-    # join video room
-    plugin = await session.attach("janus.plugin.videoroom")
-    response = await plugin.send(
-        {
-            "body": {
-                "display": display,
-                "ptype": "publisher",
-                "request": "join",
-                "room": room,
-                "pin": str(room),
-            }
-        }
-    )
-
-    publishers = response["plugindata"]["data"]["publishers"]
-    for publisher in publishers:
-        print("id: %(id)s, display: %(display)s" % publisher)
-
-    # send video
-    await publish(plugin=plugin, player=player, rtsp=rtsp)
-
-    # receive video
-    if recorder is not None and publishers:
-        await subscribe(
-            session=session, room=room, feed=publishers[0]["id"], recorder=recorder
-        )
-
-    # exchange media for 10 minutes
-    print("Exchanging media")
-    await asyncio.sleep(600)
-
 if __name__ == "__main__":
     Gst.init(None)
 
     parser = argparse.ArgumentParser(description="Janus")
-    parser.add_argument("url", help="Janus root URL, e.g. http://localhost:8088/janus")
+    parser.add_argument("url", help="Janus root URL, e.g. ws://localhost:8188")
     parser.add_argument(
         "--room",
         type=int,
@@ -296,16 +423,7 @@ if __name__ == "__main__":
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
-    # create signaling and peer connection
-    session = JanusSession(args.url)
-
-    # create media source
     play_from = args.play_from
-    if play_from:
-        if play_from.startswith("rtsp") == False:
-            player = MediaPlayer(args.play_from)
-    else:
-        player = None
 
     # create media sink
     if args.record_to:
@@ -313,10 +431,17 @@ if __name__ == "__main__":
     else:
         recorder = None
 
+    # create signaling client
+    signaling = JanusGateway("ws://192.168.5.12:8188")
+
+    # create webrtc client
+    our_id = random.randrange(10, 10000)
+    rtc_client = WebRTCClient(our_id, signaling, play_from)
+
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(
-            run(player=None, rtsp=play_from, recorder=recorder, room=args.room, session=session, display=args.name)
+             rtc_client.loop(signaling=signaling, room=args.room, display=args.name)
         )
     except KeyboardInterrupt:
         pass
@@ -324,12 +449,8 @@ if __name__ == "__main__":
         print("Stopping now!")
         if recorder is not None:
             loop.run_until_complete(recorder.stop())
-        loop.run_until_complete(session.destroy())
 
-        # close peer connections
-        coros = [pc.close() for pc in pcs]
-        loop.run_until_complete(asyncio.gather(*coros))
-
-        if camera is not None:
-            camera.stop()
-            del camera
+        # 销毁 RTC client
+        loop.run_until_complete(rtc_client.destroy())
+        # 关闭 WS
+        loop.run_until_complete(signaling.close())
