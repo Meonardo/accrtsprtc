d77fcf4098ec42b1276c0f2d4601f07129f877dd
Hedi <hedi@amdox>
2021-07-16 10:48:18 +0800

    Remove unused class.

--- janus.py
+++ janus.py
@@ -6,7 +6,6 @@ import string
 import websockets
 import json
 import attr
-import cv2
 
 import gi
 gi.require_version('Gst', '1.0')
@@ -15,7 +14,7 @@ from gi.repository import Gst
 from websockets.exceptions import ConnectionClosed
 
 from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaPlayer, MediaRecorder, MediaRelay
+from aiortc.contrib.media import MediaPlayer, MediaRecorder
 from aiortc.mediastreams import MediaStreamTrack
 
 from collections import OrderedDict
@@ -29,7 +28,7 @@ codec_parameters = OrderedDict(
     [
         ("packetization-mode", "1"),
         ("level-asymmetry-allowed", "1"),
-        ("profile-level-id", "42001f"),
+        ("profile-level-id", "42e01f"),
     ]
 )
 h264_capability = RTCRtpCodecCapability(
@@ -346,78 +345,6 @@ class GstH264Camera:
 
     def stop(self):
         self.pipeline.set_state(Gst.State.NULL)
-
-class VideoTransformTrack(MediaStreamTrack):
-    """
-    A video stream track that transforms frames from an another track.
-    """
-
-    kind = "video"
-
-    def __init__(self, track, transform):
-        super().__init__()  # don't forget this!
-        self.track = track
-        self.transform = transform
-        print("Video Transform Begins!")
-
-    async def recv(self):
-        frame = await self.track.recv()
-        print("Transforming...")
-
-        if self.transform == "cartoon":
-            img = frame.to_ndarray(format="bgr24")
-
-            # prepare color
-            img_color = cv2.pyrDown(cv2.pyrDown(img))
-            for _ in range(6):
-                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
-
-            # prepare edges
-            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            img_edges = cv2.adaptiveThreshold(
-                cv2.medianBlur(img_edges, 7),
-                255,
-                cv2.ADAPTIVE_THRESH_MEAN_C,
-                cv2.THRESH_BINARY,
-                9,
-                2,
-            )
-            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
-
-            # combine color and edges
-            img = cv2.bitwise_and(img_color, img_edges)
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        elif self.transform == "edges":
-            # perform edge detection
-            img = frame.to_ndarray(format="bgr24")
-            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        elif self.transform == "rotate":
-            # rotate image
-            img = frame.to_ndarray(format="bgr24")
-            rows, cols, _ = img.shape
-            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-            img = cv2.warpAffine(img, M, (cols, rows))
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        else:
-            return frame
-
 
 if __name__ == "__main__":
     Gst.init(None)
